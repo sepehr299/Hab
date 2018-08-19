c28f4e84d7d17a52c25dd9ab66308abca6434ae9
sepehr299 <42510896+sepehr299@users.noreply.github.com>
2018-08-19 17:38:08 +0430

    Update hackinsta.py

--- hackinsta.py
+++ hackinsta.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import codecs
 import time
-
+1
 base_url = 'https://www.instagram.com'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
 
